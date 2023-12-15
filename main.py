5701837e0dbd3f44e21ce51aca1f726f96e2c2ef
gds4 <132202830+gds4@users.noreply.github.com>
2023-12-15 12:47:06 +0000

    commit changes

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    return render_template('tela_inicial.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
